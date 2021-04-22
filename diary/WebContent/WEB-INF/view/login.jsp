da80c046c71c32e312ea51a0f4e7241be8c53feb
GDJ34 <GDJ34@DESKTOP-U9UQ47E>
2021-04-22 14:47:11 +0900

    Service / Query / Dao LoginController 구현 LogoutController 구현

--- diary/WebContent/WEB-INF/view/login.jsp
+++ diary/WebContent/WEB-INF/view/login.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,25 @@
 <title>login</title>
 </head>
 <body>
-	<h1>login</h1>
+	<!-- 로그인 전 -->
+	<c:if test="${sessionMember == null}">
+		<h1>login</h1>
+		<form action="${pageContext.request.contextPath}/login" method="post">
+			<div>
+				<div>Id : </div>
+				<div><input type="text" name="memberId" value="goodee@gdu.co.kr"></div>
+				<div>Pw : </div>
+				<div><input type="password" name="memberPw" value="1234"></div>
+				<button type="submit">로그인</button>
+			</div>
+		</form>
+	</c:if>
+	
+	<!-- 로그인 후 -->
+	<c:if test="${sessionMember != null}">
+		${sessionMember.memberId} 님 반갑습니다.
+		<div><a href="/auth/">다이어리</a></div>
+		<div><a href="${pageContext.request.contextPath}/auth/logout">로그아웃</a></div>
+	</c:if>
 </body>
 </html>
--- diary/src/gdu/diary/controller/LoginController.java
+++ diary/src/gdu/diary/controller/LoginController.java
@@ -1,16 +1,47 @@
 package gdu.diary.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import gdu.diary.service.MemberService;
+import gdu.diary.vo.Member;
 
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
 
+	private MemberService memberService;
+	
+	// 로그인 폼
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+	}
+	
+	// 로그인 액션
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String memberId = request.getParameter("memberId");
+		String memberPw = request.getParameter("memberPw");
+		this.memberService = new MemberService();
+		Member member = new Member();
+		member.setMemberId(memberId);
+		member.setMemberPw(memberPw);
+		System.out.println("member : " + member);
+		
+		Member returnMember = this.memberService.getMemberKey(member);
+		if(returnMember == null) {
+			System.out.println("로그인 실패");
+		} else {
+			System.out.println("로그인 성공");
+			HttpSession session = request.getSession();
+			session.setAttribute("sessionMember", returnMember);
+		}
+		response.sendRedirect(request.getContextPath()+"/login");
 	}
 }
